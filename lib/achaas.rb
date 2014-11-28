7b8c6b09869dace4390881a513e16b55d4228f89
Eloy Espinaco <eloyesp@gmail.com>
2014-11-28 11:04:09 -0300

    Require a provider in the post.

--- lib/achaas.rb
+++ lib/achaas.rb
@@ -4,6 +4,9 @@ class Achaas < Grape::API
   version 'v1'
   resource :batches do
     desc 'send transactions to some provider'
+    params do
+      requires :provider
+    end
     post do
       'ok'
     end
--- test/sending_transactions.rb
+++ test/sending_transactions.rb
@@ -3,6 +3,12 @@ require 'pp'
 
 include Rack::Test::Methods
 
+I18n.enforce_available_locales = false
+
+prepare do
+  header 'Content-Type', 'application/json'
+end
+
 def app
   Achaas
 end
@@ -13,6 +19,33 @@ def assert bool, *rest
 end
 
 test 'POST /v1/batches' do
+  post '/v1/batches', <<-JSON
+    {
+        "provider": "mock_provider",
+        "transactions": [
+            {
+                "id": "FD00AFA8A0F7",
+                "transaction_type": "debit",
+                "amount": "16",
+                "effective_date": "2013-03-26",
+                "first_name": "marge",
+                "last_name": "baker",
+                "address": "101 2nd st",
+                "city": "wellsville",
+                "state": "KS",
+                "postal_code": "66092",
+                "telephone": "5858232966",
+                "account_type": "checking",
+                "routing_number": "103100195",
+                "account_number": "3423423253234"
+            }
+        ]
+    }
+  JSON
+  assert last_response.successful?
+end
+
+test 'a provider needs to be passed' do
   post '/v1/batches', <<-JSON
     {
         "transactions": [
@@ -35,5 +68,5 @@ test 'POST /v1/batches' do
         ]
     }
   JSON
-  assert last_response.successful?
+  assert last_response.bad_request?
 end
